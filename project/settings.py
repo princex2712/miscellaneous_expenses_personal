c9c6e0fd01fe06aa52cf6ca637549703b9121355
Prince Patel <pp9882602@gmail.com>
2024-04-05 08:58:18 +0530

    complete

--- project/settings.py
+++ project/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email Credential
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pp9882602@gmail.com'
-EMAIL_HOST_PASSWORD = 'ooejeluecfdbrkvv'
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT =os.environ.get('EMAIL_PORT')
